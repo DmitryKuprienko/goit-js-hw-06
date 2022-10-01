b70630384a9c762b5e66504499dac21714e4d718
DmitryKuprienko <101987031+DmitryKuprienko@users.noreply.github.com>
2022-10-01 22:43:42 +0300

    second push

---
+++ src/js/task-01.js
@@ -0,0 +1,9 @@
+// console.log(document);
+const navEL = document.querySelector(`.item`);
+// console.log(navEL);
+
+const navItemEL = document.querySelectorAll(`.item`);
+// console.log(`Number of categories:`, navItemEL);
+
+const allH2 = document.getElementsByClassName(`item-title`);
+console.log(allH2);
