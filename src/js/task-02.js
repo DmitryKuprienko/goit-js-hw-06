8957ecb480bc931717eb77de5455f15d201a2508
DmitryKuprienko <101987031+DmitryKuprienko@users.noreply.github.com>
2022-10-04 17:39:38 +0300

    2 завдвння виконана

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,23 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+// Create any element algorythm
+// 1. document.createElement()
+// 2. document.setAtribute and clases
+// 3. add Text (если нужно);
+// 4. Insert into dom(привяжи в DOM, свяжи с другим элементом)
+
+const ulNewRef = document.querySelector("#ingredients");
+const liNewRef = ingredients
+  .map((ingredient) => `<li class="list-item">${ingredient}</li>`)
+  .join("");
+
+// console.log(liNewRef);
+ulNewRef.innerHTML = liNewRef;
+console.log(ulNewRef);
