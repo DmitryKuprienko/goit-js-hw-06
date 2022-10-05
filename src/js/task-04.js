ece4c3a4884dc106a0b11346d1feceb5f12e07fb
DmitryKuprienko <101987031+DmitryKuprienko@users.noreply.github.com>
2022-10-05 23:58:42 +0300

    4 виконано

---
+++ src/js/task-04.js
@@ -0,0 +1,25 @@
+const decBtnRef= document.querySelector(`[data-action="decrement"]`);
+
+const incBtnRef= document.querySelector(`[data-action="increment"]`);
+const counterRef  = document.querySelector(`#value`)
+// console.log(decBtnRef)
+// console.log(incBtnRef)
+// console.log(counterRef)
+let counterValue = 0 ;
+
+
+
+
+
+incBtnRef.addEventListener('click', () => {
+    counterValue += Number(incBtnRef.textContent);
+    console.log(counterValue)
+    counterRef.textContent = String(counterValue);
+    console.log(counterRef)
+});
+decBtnRef.addEventListener('click', () => {
+    counterValue += Number(decBtnRef.textContent);
+    console.log(counterValue)
+    counterRef.textContent = String(counterValue);
+    console.log(counterRef)
+});
