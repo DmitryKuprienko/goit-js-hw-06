3fee8fcc6e1941caf551be6a14d42a65613eedd4
DmitryKuprienko <101987031+DmitryKuprienko@users.noreply.github.com>
2022-10-13 21:45:37 +0300

    task 09 compited

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,23 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16).toString(16)
+    .padStart(6, 0)}`;
 }
+
+
+const buttonRef =document.querySelector("button.change-color")
+const spanRef= document.querySelector(".color")
+
+
+buttonRef.addEventListener("click",changeBackgroundColorClick)
+
+function changeBackgroundColorClick() {
+  const newColor = getRandomHexColor()
+  const bodyRef=document.body
+  
+  bodyRef.style.backgroundColor = newColor;
+  spanRef.textContent = `${newColor}`
+  // console.dir( spanRef)
+}
+
+
+
