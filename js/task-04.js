035b6e0fe27401123ea440aea95bac9c83d4a5c7
DmitryKuprienko <101987031+DmitryKuprienko@users.noreply.github.com>
2024-01-31 22:27:34 +0200

    на 5 завданні зробив 4те

--- js/task-04.js
+++ js/task-04.js
@@ -1,20 +1,21 @@
-const decBtnRef= document.querySelector(`[data-action="decrement"]`);
+const decBtnRef = document.querySelector(`[data-action="decrement"]`);
 
-const incBtnRef= document.querySelector(`[data-action="increment"]`);
-const counterRef  = document.querySelector(`#value`)
+const incBtnRef = document.querySelector(`[data-action="increment"]`);
+const counterRef = document.querySelector(`#value`);
 // console.log(decBtnRef)
 // console.log(incBtnRef)
 // console.log(counterRef)
-let counterValue = 0 ;
+let counterValue = 0;
+
 incBtnRef.addEventListener('click', () => {
-    counterValue += Number(incBtnRef.textContent);
-    // console.log(counterValue)
-    counterRef.textContent = String(counterValue);
-    // console.log(counterRef)
+  counterValue += Number(incBtnRef.textContent);
+  // console.log(counterValue)
+  counterRef.textContent = String(counterValue);
+  // console.log(counterRef)
 });
 decBtnRef.addEventListener('click', () => {
-    counterValue += Number(decBtnRef.textContent);
-    // console.log(counterValue)
-    counterRef.textContent = String(counterValue);
-    // console.log(counterRef)
+  counterValue += Number(decBtnRef.textContent);
+  // console.log(counterValue)
+  counterRef.textContent = String(counterValue);
+  // console.log(counterRef)
 });
